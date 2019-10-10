82f4132180b1a38174c81aafc2108b39d32d53a6
Franz <pfquispe@gmail.com>
2019-10-09 23:32:31 -0400

    Added new layout.

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.myfirstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.SeekBar
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton = findViewById<Button>(R.id.rollButton)
+        val resultsTextView = findViewById<TextView>(R.id.resultsTextView)
+        val seekBar = findViewById<SeekBar>(R.id.seekBar)
     }
 }
